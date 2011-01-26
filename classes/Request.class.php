9227863f5c0c0dbf16f2563042fd65f4b9be6784
macbre <macbre@7f084d9a-2024-4ba8-ae8f-f737d1f944ca>
2011-01-26 23:19:38 +0000

    nano:  * klasa Request + testy

---
+++ classes/Request.class.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * Wrapper for HTTP request
+ *
+ * $Id$
+ */
+
+class Request {
+
+	const GET = 1;
+	const POST = 2;
+
+	// stores request parameters as [param name] => [value] keys
+	private $params;
+
+	// stores request's info as [param name] => [value] keys
+	private $info;
+
+	// HTTP request type (either GET or POST)
+	private $type;
+
+	/**
+	 * Setup request object
+	 */
+	public function __construct(array $request, array $server) {
+		$this->params = $request;
+		$this->info = $server;
+
+		// detect request type
+		switch($server['REQUEST_METHOD']) {
+			case 'POST':
+				$this->type = self::POST;
+				break;
+
+			case 'GET':
+			default:
+				$this->type = self::GET;
+				break;
+		}
+	}
+
+	/**
+	 * Get value of given request parameter
+	 *
+	 * Return $default if parameter is not found
+	 */
+	public function get($param, $default = null) {
+		return isset($this->params[$param]) ? $this->params[$param] : $default;
+	}
+
+	/**
+	 * Get numeric value of given request parameter
+	 *
+	 * Return $default if parameter is not found
+	 */
+	public function getInt($param, $default = 0) {
+		return intval($this->get($param, $default));
+	}
+
+	/**
+	 * Return if given checkbox is selected
+	 */
+	public function getChecked($param) {
+		return $this->get($param) == 'on';
+	}
+
+	/**
+	 * Return if current request was sent using POST method
+	 */
+	public function wasPosted() {
+		return $this->type == self::POST;
+	}
+
+	/**
+	 * Get IP address of client
+	 *
+	 * Return null if IP is malformed or not provided (CLI?)
+	 */
+	public function getIP() {
+		$ip = false;
+
+		if (!empty($this->info['HTTP_CLIENT_IP'])) {
+			$ip = $this->info['HTTP_CLIENT_IP'];
+		}
+
+		if (!empty($this->info['REMOTE_ADDR'])) {
+			$ip = $this->info['REMOTE_ADDR'];
+		}
+
+		// proxy
+		if (!empty($this->info['HTTP_X_FORWARDED_FOR'])) {
+			$ipList = explode (', ', $this->info['HTTP_X_FORWARDED_FOR']);
+
+			if ($ip) {
+				$ipList[] = $ip;
+				$ip = false;
+			}
+
+			// scan IP addresses for local ones (ignore them)
+			foreach ($ipList as $v) {
+				if (!preg_match('#^(192\.168|172\.16|10|224|240|127|0)\.#', $v)) {
+					return $v;
+				}
+			}
+		}
+
+		return $ip ? $ip : null;
+	}
+}
---
+++ tests/RequestTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Set of unit tests for Request class
+ *
+ * $Id$
+ */
+
+class RequestTest extends PHPUnit_Framework_TestCase {
+
+	public function testGETParams() {
+		// emulate GET request
+		$request = new Request(array(
+			'foo' => 'bar',
+			'test' => '2',
+			'box' => 'on',
+		),
+		array(
+			'REQUEST_METHOD' => 'GET'
+		));
+
+		$this->assertFalse($request->wasPosted());
+
+		$this->assertNull($request->get('foo2'));
+		$this->assertEquals('bar', $request->get('foo'));
+		$this->assertEquals(0, $request->getInt('foo'));
+		$this->assertEquals(2, $request->getInt('test'));
+		$this->assertTrue($request->getChecked('box'));
+		$this->assertFalse($request->getChecked('box2'));
+	}
+
+	public function testPOSTParams() {
+		// emulate POST request
+		$request = new Request(array(
+			'foo' => 'bar',
+			'box' => 'on',
+		),
+		array(
+			'REQUEST_METHOD' => 'POST'
+		));
+
+		$this->assertTrue($request->wasPosted());
+
+		$this->assertNull($request->get('test'));
+		$this->assertEquals('bar', $request->get('foo'));
+		$this->assertEquals(0, $request->getInt('foo'));
+		$this->assertTrue($request->getChecked('box'));
+		$this->assertFalse($request->getChecked('box2'));
+	}
+
+	public function testIP() {
+		// crawl-66-249-66-248.googlebot.com
+		$ip = '66.249.66.248';
+
+		$request = new Request(array(), array('HTTP_CLIENT_IP' => $ip));
+		$this->assertEquals($ip, $request->getIP());
+
+		$request = new Request(array(), array('REMOTE_ADDR' => $ip));
+		$this->assertEquals($ip, $request->getIP());
+
+		$request = new Request(array(), array('HTTP_X_FORWARDED_FOR' => $ip));
+		$this->assertEquals($ip, $request->getIP());
+
+		$request = new Request(array(), array('HTTP_X_FORWARDED_FOR' => "192.168.0.1, {$ip}"));
+		$this->assertEquals($ip, $request->getIP());
+	}
+}
