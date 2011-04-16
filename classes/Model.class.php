8d567b19055ac1f10bfa5ab0b8dcf5b6ae1c48dd
Maciej Brencz <macbre@elecena.pl>
2011-04-16 10:58:27 +0000

    nano: abstrakcyjna klasa dla modeli

---
+++ classes/Model.class.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Abstract class for representing nanoPortal's application model
+ *
+ * $Id$
+ */
+
+abstract class Model {
+
+	// application
+	protected $app;
+
+	// cache object
+	protected $cache;
+
+	// debug
+	protected $debug;
+
+	// DB connection
+	protected $database;
+
+	// config
+	protected $config;
+
+	// events handler
+	protected $events;
+
+	/**
+	 * Use given application
+	 */
+	private function __construct(NanoApp $app) {
+		$this->app = $app;
+
+		$this->cache = $app->getCache();
+		$this->config = $app->getConfig();
+		$this->database = $app->getDatabase();
+		$this->debug = $app->getDebug();
+		$this->events = $app->getEvents();
+	}
+}
