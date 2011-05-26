834bcb7361595ec0de0d0caeb2636c03f081a26d
Maciej Brencz <macbre@elecena.pl>
2011-05-26 19:17:49 +0000

    nano: dodana klasa ResultsWrapper

---
+++ classes/ResultsWrapper.class.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Wrapper for results
+ *
+ * $Id$
+ */
+
+class ResultsWrapper {
+
+	// results
+	protected $results;
+
+	function __construct($results) {
+		$this->results = $results;
+	}
+
+	/**
+	 * Set given entry in results array
+	 *
+	 * Key should be lowercase
+	 */
+	public function set($key, $value) {
+		$this->results[$key] = $value;
+	}
+
+	/**
+	 * Get given results entry
+	 *
+	 * To get 'data' entry use $res->getData()
+	 */
+	public function __call($name, $parameters) {
+		$res = null;
+
+		// getXXX
+		if (substr($name, 0, 3) == 'get') {
+			$entry = strtolower(substr($name, 3));
+
+			if (isset($this->results[$entry])) {
+				$res = $this->results[$entry];
+			}
+		}
+
+		return $res;
+	}
+}
---
+++ tests/ResultsWrapperTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Set of unit tests for ResultsWrapper class
+ *
+ * $Id$
+ */
+
+class ResultsWrapperTest extends PHPUnit_Framework_TestCase {
+
+	public function testSetGet() {
+		$data = array(
+			'foo' => 'bar',
+			'id' => 123,
+		);
+
+		$res = new ResultsWrapper($data);
+		$res->set('test', 'abc');
+
+		$this->assertEquals($data['foo'], $res->getFoo());
+		$this->assertEquals($data['id'], $res->getId());
+		$this->assertEquals('abc', $res->getTest());
+	}
+}
