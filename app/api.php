8510560c78651463b5c7ac099b48d4aa76d536a0
Maciej Brencz <macbre@elecena.pl>
2011-08-06 21:20:36 +0000

    nano: formatuj odpowiedzi z API aplikacji

--- app/api.php
+++ app/api.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * nanoPortal application  entry point for API requests
+ * nanoPortal application entry point for API requests
  *
  * $Id$
  */
@@ -9,8 +9,45 @@
 include 'app.php';
 
 $request = $app->getRequest();
+$response = $app->getResponse();
 
 // request comes from API dispatcher
 $request->setType(Request::API);
 
-var_dump(__FILE__); var_dump($request); die(); // debug
+// TODO: parse request path
+$path = $request->getPath();
+$format = 'json';
+
+$parts = explode('.', $path, 2);
+
+if (count($parts) == 2) {
+	$request->setPath($parts[0]);
+	$format = $parts[1];
+}
+
+// support JSONP format
+if ($format == 'json') {
+	$callback = $request->get('callback');
+
+	if (!empty($callback)) {
+		$format = 'jsonp';
+	}
+}
+
+// dispatch the request
+$data = $app->dispatchRequest($request);
+
+$data = array('foo' => 'bar');
+
+// format the response
+$output = Output::factory($format, $data);
+
+if (!empty($callback)) {
+	$output->setCallback($callback);
+}
+
+// generate the response
+$response->setResponseCode(Response::OK);
+$response->setContent($output);
+
+var_dump(__FILE__); var_dump($request); var_dump($data); var_dump($response); die(); // debug
