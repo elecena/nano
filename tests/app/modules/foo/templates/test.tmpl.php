2d9d5e6247a64d1a6367573e261e7736aec7eed3
Maciej Brencz <macbre@elecena.pl>
2011-08-12 17:22:20 +0000

    nano: svn:eol-style = native

--- tests/app/modules/foo/templates/test.tmpl.php
+++ tests/app/modules/foo/templates/test.tmpl.php
@@ -1,1 +1,16 @@
-<h1><?= htmlspecialchars($title) ?></h1><?php	if (!empty($items) && is_array($items)) {?><ul><?php	foreach($items as $item) {?>	<li><?= htmlspecialchars($item)  ?></li><?php	}?></ul><?php	}?>
+<h1><?= htmlspecialchars($title) ?></h1>
+<?php
+	if (!empty($items) && is_array($items)) {
+?>
+<ul>
+<?php
+	foreach($items as $item) {
+?>
+	<li><?= htmlspecialchars($item)  ?></li>
+<?php
+	}
+?>
+</ul>
+<?php
+	}
+?>
