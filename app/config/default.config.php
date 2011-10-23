7951d2e880775218c54718fef0b0eb74d9d5c89d
Maciej Brencz <macbre@elecena.pl>
2011-10-23 19:26:26 +0000

    nano: aplikacja przykładowa (paczki plików statycznych)

---
+++ app/config/default.config.php
@@ -0,0 +1,14 @@
+<?php
+
+$config['assets.packages'] = array(
+	'js' => array(
+		'app' => array(
+			'/statics/head.js',
+		),
+	),
+	'css' => array(
+		'styles' => array(
+			'/statics/reset.css',
+		),
+	),
+);
