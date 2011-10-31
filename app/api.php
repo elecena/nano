b05b5be1fa1610288cc6620057ac4047993f7f2b
Maciej Brencz <macbre@elecena.pl>
2011-10-31 18:11:00 +0000

    nano: API:  * zwracaj błąd 404  * poprawione parsowanie rozszerzeń (json/xml)

--- app/api.php
+++ app/api.php
@@ -14,22 +14,25 @@ $response = $app->getResponse();
 // request comes from API dispatcher
 $request->setType(Request::API);
 
-// TODO: parse request path
+// parse request path to get response format
 $path = $request->getPath();
-$format = 'json';
+$format = $request->getExtension();
 
-$parts = explode('.', $path, 2);
-
-if (count($parts) == 2) {
-	$request->setPath($parts[0]);
-	$format = $parts[1];
+if (!is_null($format)) {
+	// remove extension part from request path
+	$path = reset(explode('.', $path, 2));
+	$request->setPath($path);
+}
+else {
+	// default format if not specified
+	$format = 'json';
 }
 
 // support JSONP format
 if ($format == 'json') {
-	$callback = $request->get('callback');
+	$callback = trim($request->get('callback', ''));
 
-	if (!empty($callback)) {
+	if ($callback != '') {
 		$format = 'jsonp';
 	}
 }
@@ -37,19 +40,19 @@ if ($format == 'json') {
 // dispatch the request
 $data = $app->dispatchRequest($request);
 
-$data = array('foo' => 'bar');
+if ($data !== false) {
+	// format the response
+	$output = Output::factory($format, $data);
 
-// format the response
-$output = Output::factory($format, $data);
+	if (!empty($callback)) {
+		$output->setCallback($callback);
+	}
 
-if (!empty($callback)) {
-	$output->setCallback($callback);
+	// generate the response
+	$response->setResponseCode(Response::OK);
+	$response->setContent($output);
 }
-
-// generate the response
-$response->setResponseCode(Response::OK);
-$response->setContent($output);
 
 echo $response->render();
 
-//var_dump(__FILE__); var_dump($request); var_dump($data); var_dump($response); die(); // debug
+#var_dump(__FILE__); var_dump($format); var_dump($request); var_dump($data); var_dump($response); die(); // debug
