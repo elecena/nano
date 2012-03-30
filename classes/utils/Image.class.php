5fcb6c9439d246ae878bb9585c8e4e432336ce49
Maciej Brencz <macbre@elecena.pl>
2012-03-30 19:48:28 +0000

    nano: dodaj sterownik ImageGD

--- classes/utils/Image.class.php
+++ classes/utils/Image.class.php
@@ -8,34 +8,21 @@
  * $Id$
  */
 
-class Image {
+abstract class Image {
 	// image handler from GD / Imagick
-	private $img;
+	protected $img;
 
 	// image type
-	private $type;
+	protected $type;
 
 	// image dimensions
-	private $width;
-	private $height;
-
-	function __construct($img) {
-		$this->img = $img;
-
-		if (is_resource($this->img)) {
-			$this->width = imagesx($this->img);
-			$this->height = imagesy($this->img);
-		}
-	}
+	protected $width;
+	protected $height;
 
 	/**
-	 * Returns an instance of proper image driver
-	 *
-	 * Auto-detects which library to use: Imagick or GD
+	 * Create an instance of Image for given raw image data
 	 */
-	public function getInstance() {
-		// TODO
-	}
+	abstract function __construct($raw);
 
 	/**
 	 * Create an instance of Image from given URL (file will be fetched via HTTP)
@@ -60,12 +47,11 @@ class Image {
 	 */
 	public static function newFromRaw($raw) {
 		if (!empty($raw)) {
-
-			// TODO: use self::getInstance
-			$img = imagecreatefromstring($raw);
-
-			if (is_resource($img)) {
-				return new self($img);
+			try {
+				return self::getInstance($raw);
+			}
+			catch(Exception $e) {
+				return false;
 			}
 		}
 
@@ -73,154 +59,40 @@ class Image {
 	}
 
 	/**
+	 * Returns an instance of proper image driver
+	 *
+	 * Auto-detects which library to use: Imagick or GD
+	 */
+	private static function getInstance($raw) {
+		// TODO: Imagick
+		if (false) {
+			$driverName = 'ImageImagick';
+		}
+		// fallback to GD
+		else if (function_exists('gd_info')){
+			$driverName = 'ImageGD';
+		}
+		else {
+			return false;
+		}
+
+		return new $driverName($raw);
+	}
+
+	/**
 	 * Scale an image to fit given box
 	 */
-	public function scale($width, $height) {
-		// calculate scale-down ratio
-		$ratio = min($width / $this->width, $height / $this->height);
-
-		// don't scale up
-		if ($ratio >= 1) {
-			return false;
-		}
-
-		// new dimensions
-		$width = round($this->width * $ratio);
-		$height = round($this->height * $ratio);
-
-		// create new image
-		$thumb = ImageCreateTrueColor($width, $height);
-
-		if (!is_resource($thumb)) {
-			return false;
-		}
-
-		// paste rescaled image
-		// @see http://pl.php.net/imageCopyResampled
-		$res = imageCopyResampled(
-			// destination
-			$thumb,
-			// source
-			$this->img,
-			// destination upper left coordinates
-			0, 0,
-			// source upper left coordinates
-			0, 0,
-			// destination dimensions
-			$width, $height,
-			// source dimensions
-			$this->width, $this->height
-		);
-
-		if ($res === false) {
-			return true;
-		}
-
-		// free memory
-		imagedestroy($this->img);
-
-		// update image data
-		$this->img = $thumb;
-		$this->width = $width;
-		$this->height = $height;
-
-		return true;
-	}
+	abstract public function scale($width, $height) ;
 
 	/**
 	 * Crop an image to fit given box
 	 */
-	public function crop($width, $height) {
-		// calculate scale-down ratio
-		$ratio = max($width / $this->width, $height / $this->height);
-
-		// don't scale up
-		if ($ratio >= 1) {
-			return false;
-		}
-
-		// create new image
-		$thumb = ImageCreateTrueColor($width, $height);
-
-		if (!is_resource($thumb)) {
-			return false;
-		}
-
-		// 500x300 (src) -> 333x200
-		// 200x200 (dest)
-		//  ratio = 2/3
-		// dst_x = 333 - 200 / 2 -> ((500 x 2/3) - 200) / 2
-		// dst_y = 200 - 200 / 2 -> ((200 x 2/3) - 200) / 2
-
-		// paste rescaled image
-		// @see http://pl.php.net/imageCopyResampled
-		$res = imageCopyResampled(
-			// destination
-			$thumb,
-			// source
-			$this->img,
-			// destination upper left coordinates
-			round(($this->width * $ratio - $width) / 2) * -1,
-			round(($this->height * $ratio - $height) / 2) * -1,
-			// source upper left coordinates
-			0, 0,
-			// destination dimensions
-			round($this->width * $ratio), round($this->height * $ratio),
-			// source dimensions
-			$this->width, $this->height
-		);
-
-		if ($res === false) {
-			return true;
-		}
-
-		// free memory
-		imagedestroy($this->img);
-
-		// update image data
-		$this->img = $thumb;
-		$this->width = $width;
-		$this->height = $height;
-
-		return true;
-	}
+	abstract public function crop($width, $height);
 
 	/**
 	 * Return image raw data
 	 */
-	public function render($type, $quality = false) {
-		ob_start();
-
-		switch($type) {
-			case 'jpeg':
-				$type = IMAGETYPE_JPEG;
-				imagejpeg($this->img, null, $quality ? $quality : 75);
-				break;
-
-			case 'gif':
-				$type = IMAGETYPE_GIF;
-				imagegif($this->img);
-				break;
-
-			case 'png':
-				$type = IMAGETYPE_PNG;
-				imagepng($this->img, null, $quality ? $quality : 9);
-				break;
-
-			default:
-				return false;
-		}
-
-		// get an image
-		$raw = ob_get_contents();
-		ob_end_clean();
-
-		if (!empty($raw)) {
-			$this->type = $type;
-		}
-
-		return $raw;
-	}
+	abstract public function render($type, $quality = false);
 
 	/**
 	 * Save image raw data to a given file
---
+++ classes/utils/ImageGD.class.php
@@ -0,0 +1,176 @@
+<?php
+
+/**
+ * Helper class for handling image manipulations using GD library
+ *
+ * @see http://www.php.net/manual/en/book.image.php
+ *
+ * $Id$
+ */
+
+class ImageGD extends Image {
+
+	/**
+	 * Create an instance of Image for given raw image data
+	 */
+	function __construct($raw) {
+		$this->img = imagecreatefromstring($raw);
+
+		if (!is_resource($this->img)) {
+			throw new Exception('imagecreatefromstring() failed!');
+		}
+
+		$this->width = imagesx($this->img);
+		$this->height = imagesy($this->img);
+	}
+
+	/**
+	 * Scale an image to fit given box
+	 */
+	public function scale($width, $height) {
+		// calculate scale-down ratio
+		$ratio = min($width / $this->width, $height / $this->height);
+
+		// don't scale up
+		if ($ratio >= 1) {
+			return false;
+		}
+
+		// new dimensions
+		$width = round($this->width * $ratio);
+		$height = round($this->height * $ratio);
+
+		// create new image
+		$thumb = ImageCreateTrueColor($width, $height);
+
+		if (!is_resource($thumb)) {
+			return false;
+		}
+
+		// paste rescaled image
+		// @see http://pl.php.net/imageCopyResampled
+		$res = imageCopyResampled(
+			// destination
+			$thumb,
+			// source
+			$this->img,
+			// destination upper left coordinates
+			0, 0,
+			// source upper left coordinates
+			0, 0,
+			// destination dimensions
+			$width, $height,
+			// source dimensions
+			$this->width, $this->height
+		);
+
+		if ($res === false) {
+			return true;
+		}
+
+		// free memory
+		imagedestroy($this->img);
+
+		// update image data
+		$this->img = $thumb;
+		$this->width = $width;
+		$this->height = $height;
+
+		return true;
+	}
+
+	/**
+	 * Crop an image to fit given box
+	 */
+	public function crop($width, $height) {
+		// calculate scale-down ratio
+		$ratio = max($width / $this->width, $height / $this->height);
+
+		// don't scale up
+		if ($ratio >= 1) {
+			return false;
+		}
+
+		// create new image
+		$thumb = ImageCreateTrueColor($width, $height);
+
+		if (!is_resource($thumb)) {
+			return false;
+		}
+
+		// 500x300 (src) -> 333x200
+		// 200x200 (dest)
+		//  ratio = 2/3
+		// dst_x = 333 - 200 / 2 -> ((500 x 2/3) - 200) / 2
+		// dst_y = 200 - 200 / 2 -> ((200 x 2/3) - 200) / 2
+
+		// paste rescaled image
+		// @see http://pl.php.net/imageCopyResampled
+		$res = imageCopyResampled(
+			// destination
+			$thumb,
+			// source
+			$this->img,
+			// destination upper left coordinates
+			round(($this->width * $ratio - $width) / 2) * -1,
+			round(($this->height * $ratio - $height) / 2) * -1,
+			// source upper left coordinates
+			0, 0,
+			// destination dimensions
+			round($this->width * $ratio), round($this->height * $ratio),
+			// source dimensions
+			$this->width, $this->height
+		);
+
+		if ($res === false) {
+			return true;
+		}
+
+		// free memory
+		imagedestroy($this->img);
+
+		// update image data
+		$this->img = $thumb;
+		$this->width = $width;
+		$this->height = $height;
+
+		return true;
+	}
+
+	/**
+	 * Return image raw data
+	 */
+	public function render($type, $quality = false) {
+		ob_start();
+
+		switch($type) {
+			case 'jpeg':
+				$type = IMAGETYPE_JPEG;
+				imagejpeg($this->img, null, $quality ? $quality : 75);
+				break;
+
+			case 'gif':
+				$type = IMAGETYPE_GIF;
+				imagegif($this->img);
+				break;
+
+			case 'png':
+				$type = IMAGETYPE_PNG;
+				imagepng($this->img, null, $quality ? $quality : 9);
+				break;
+
+			default:
+				return false;
+		}
+
+		// get an image
+		$raw = ob_get_contents();
+		ob_end_clean();
+
+		if (!empty($raw)) {
+			$this->type = $type;
+		}
+
+		return $raw;
+	}
+}
