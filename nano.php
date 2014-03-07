388e4fd6377f6970d272debb15358751b7ec516e
Maciej Brencz <macbre@elecena.pl>
2014-03-07 21:09:47 +0100

    Set $_SERVER['REQUEST_TIME_FLOAT'] in PHP 5.3.x

--- nano.php
+++ nano.php
@@ -10,5 +10,12 @@ error_reporting(E_ALL);
 // UTF-8 stuff
 mb_internal_encoding('utf-8');
 
+// compatibility code
+if (!array_key_exists($_SERVER, 'REQUEST_TIME_FLOAT')) {
+	// As of PHP 5.4.0, REQUEST_TIME_FLOAT is available in the $_SERVER superglobal array.
+	// It contains the timestamp of the start of the request with microsecond precision.
+	$_SERVER['REQUEST_TIME_FLOAT'] = microtime(true);
+}
+
 // load autoloader class
 require_once dirname(__FILE__) . '/classes/Nano.class.php';
