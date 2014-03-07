da53431ebd27cb32d7398ba07833574ec9d27984
Maciej Brencz <macbre@elecena.pl>
2014-03-07 21:24:44 +0100

    array_key_exists: arguments order

--- nano.php
+++ nano.php
@@ -11,7 +11,7 @@ error_reporting(E_ALL);
 mb_internal_encoding('utf-8');
 
 // compatibility code
-if (!array_key_exists($_SERVER, 'REQUEST_TIME_FLOAT')) {
+if (!array_key_exists('REQUEST_TIME_FLOAT', $_SERVER)) {
 	// As of PHP 5.4.0, REQUEST_TIME_FLOAT is available in the $_SERVER superglobal array.
 	// It contains the timestamp of the start of the request with microsecond precision.
 	$_SERVER['REQUEST_TIME_FLOAT'] = microtime(true);
