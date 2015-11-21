586859237a567872ffdc9345225973003fcf2c0f
Maciej Brencz <macbre@elecena.pl>
2015-11-21 13:37:16 +0100

    DatabaseMysql: log affected_rows for update / insert / delete

--- classes/database/DatabaseMysql.class.php
+++ classes/database/DatabaseMysql.class.php
@@ -191,7 +191,7 @@ class DatabaseMysql extends Database {
 		else {
 			$this->logger->info("SQL {$sql}", [
 				'method' => $method,
-				'rows' => $res instanceof mysqli_result ? $res->num_rows : -1,
+				'rows' => $res instanceof mysqli_result ? ($res->num_rows ?: $this->link->affected_rows) : -1,
 				'time' => $time * 1000 // [ms]
 			]);
 		}
