e6bbdad72f281d4c386e0a5360fa1c4f8035e4f5
macbre <maciej.brencz@bethink.pl>
2021-03-24 15:08:59 +0100

    DatabaseMysqlTest: skip when there's no database

--- tests/DatabaseMysqlTest.php
+++ tests/DatabaseMysqlTest.php
@@ -5,56 +5,64 @@ use Nano\NanoBaseTest;
 class DatabaseMysqlTest extends NanoBaseTest
 {
     /**
-     * @throws DatabaseException
+     * @var DatabaseMysql|null $database
      */
+    private $database;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->database = Database::connect($this->app, ['driver' => 'mysql', 'host' => 'localhost', 'user' => 'root', 'pass' => '', 'database' => 'test']);
+    }
+
     public function testLazyConnect()
     {
-        $database = Database::connect($this->app, ['driver' => 'mysql', 'host' => 'localhost', 'user' => 'root', 'pass' => '', 'database' => 'test']);
-        $this->assertInstanceOf(DatabaseMysql::class, $database);
+        $this->assertInstanceOf(DatabaseMysql::class, $this->database);
 
-        $this->assertFalse($database->isConnected(), 'We should not be connected yet');
+        $this->assertFalse($this->database->isConnected(), 'We should not be connected yet');
 
         /**
-        $database->query('SELECT 1 FROM dual');
-        $this->assertTrue($database->isConnected(), 'We should be connected now');
+        $this->database->query('SELECT 1 FROM dual');
+        $this->assertTrue($this->database->isConnected(), 'We should be connected now');
          **/
     }
 
     // requires server running on localhost:3306
     public function testMySqlDatabase()
     {
-        $this->markTestSkipped('Requires server running on localhost:3306');
-
-        $app = Nano::app(dirname(__FILE__) . '/app');
-        $database = Database::connect($app, ['driver' => 'mysql', 'host' => 'localhost', 'user' => 'root', 'pass' => '', 'database' => 'test']);
+        try {
+            $this->database->query('SELECT 1');
+        } catch (DatabaseException $e) {
+            $this->markTestSkipped('Requires server running on localhost:3306 - ' . $e->getMessage());
+        }
 
         // test performance data
-        $performanceData = $database->getPerformanceData();
+        $performanceData = $this->database->getPerformanceData();
         $this->assertEquals(0, $performanceData['queries']);
         $this->assertEquals(0, $performanceData['time']);
 
-        $res = $database->select('test', '*');
+        $res = $this->database->select('test', '*');
         foreach ($res as $i => $row) {
             #var_dump($row);
         }
         $res->free();
 
-        $res = $database->select('test', '*');
+        $res = $this->database->select('test', '*');
         while ($row = $res->fetchRow()) {
             #var_dump($row);
         }
         $res->free();
 
-        $row = $database->selectRow('test', '*', ['id' => 2]);
+        $row = $this->database->selectRow('test', '*', ['id' => 2]);
         #var_dump($row);
 
-        $row = $database->selectField('test', 'count(*)');
+        $row = $this->database->selectField('test', 'count(*)');
         #var_dump($row);
 
-        $res = $database->query('SELECT VERSION()');
+        $res = $this->database->query('SELECT VERSION()');
         #var_dump($res->fetchField());
 
-        $performanceData = $database->getPerformanceData();
+        $performanceData = $this->database->getPerformanceData();
         $this->assertEquals(5, $performanceData['queries']);
         $this->assertTrue($performanceData['time'] > 0);
     }
