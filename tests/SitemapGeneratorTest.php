4f3753829d65b8aa3ab55e717de69179e18bb101
macbre <maciej.brencz@gmail.com>
2023-05-22 17:56:47 +0100

    Add SitemapGeneratorTest

--- tests/SitemapGeneratorTest.php
+++ tests/SitemapGeneratorTest.php
@@ -3,23 +3,46 @@
 /** use the same namespace as the @see \Nano\Utils\SitemapGenerator is in */
 namespace Nano\Utils;
 
-use Nano\NanoBaseTest;
+use Nano\AppTests\AppTestBase;
 
 /**
  * @covers \Nano\Utils\SitemapGenerator
  */
-class SitemapGeneratorTest extends NanoBaseTest
+class SitemapGeneratorTest extends AppTestBase
 {
+    public static array $filesWritten = [];
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        static::$filesWritten = [];
+    }
+
     public function testSaveXml()
     {
         $sitemap = new SitemapGenerator();
         $sitemap->startSitemap('pages');
         $sitemap->addUrl('/foo/bar');
+        $urlsCount = $sitemap->save();
+
+        $this->assertEquals(1, $urlsCount);
+
+        // assert the given files were saved
+        $this->assertCount(2, static::$filesWritten);
+        $this->assertArrayHasKey('sitemap.xml', static::$filesWritten);
+        $this->assertArrayHasKey('sitemap-001-pages.xml.gz', static::$filesWritten);
+
+        $sitemapIndexXml = (string) static::$filesWritten['sitemap.xml'];
+
+        $this->assertStringContainsString('<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">', $sitemapIndexXml);
+        $this->assertStringContainsString('<loc>http://example.org/site/sitemap-001-pages.xml.gz</loc>', $sitemapIndexXml);
     }
 }
 
 function file_put_contents(string $fileName, string $content): int
 {
-    var_dump(__METHOD__, $fileName, $content);
+    //    var_dump(__METHOD__, $fileName, $content);
+
+    SitemapGeneratorTest::$filesWritten[ basename($fileName) ] = $content;
     return strlen($content);
 }
