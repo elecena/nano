25207c30254a5b80584ec48101cb017e7adf43ba
macbre <maciej.brencz@gmail.com>
2023-08-10 13:11:35 +0100

    More tests in NanoScriptTest

--- tests/NanoScriptTest.php
+++ tests/NanoScriptTest.php
@@ -2,7 +2,7 @@
 
 use Nano\Logger\NanoLogger;
 
-class TestScript extends NanoScript
+class FailingTestScript extends NanoScript
 {
     private ?array $foo = null;
 
@@ -16,16 +16,39 @@ class TestScript extends NanoScript
     {
     }
 }
+
+class WorkingTestScript extends FailingTestScript
+{
+    public static ?Throwable $throw = null;
+
+    protected function init(): int
+    {
+        return 0;
+    }
+
+    /**
+     * @return int
+     * @throws Throwable
+     */
+    public function run(): int
+    {
+        if (self::$throw) {
+            throw self::$throw;
+        }
+        return 42;
+    }
+}
+
 class NanoScriptTest extends \Nano\NanoBaseTest
 {
-    public function testInitExceptions()
+    public function testFailingTestScriptInitException()
     {
         // register a global logging handler for easier testing
         $handler = new Nano\TestLoggingHandler(ident: 'foo');
         NanoLogger::pushHandler($handler);
 
         try {
-            Nano::script(__DIR__ . '/..', TestScript::class);
+            Nano::script(__DIR__ . '/..', FailingTestScript::class);
         } catch (Throwable) {
         }
 
@@ -33,7 +56,23 @@ class NanoScriptTest extends \Nano\NanoBaseTest
         $this->assertInstanceOf(\Monolog\LogRecord::class, $handler->lastRecord);
         $this->assertEquals(\Monolog\Level::Error, $handler->lastRecord->level);
 
-        $this->assertEquals('TestScript::init() failed', $handler->lastRecord->message);
+        $this->assertEquals('FailingTestScript::init() failed', $handler->lastRecord->message);
         $this->assertInstanceOf(TypeError::class, $handler->lastRecord->context['exception']);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testWorkingTestScript()
+    {
+        $instance = Nano::script(__DIR__ . '/..', WorkingTestScript::class);
+        $this->assertInstanceOf(WorkingTestScript::class, $instance);
+        $this->assertEquals(42, $instance->run());
+
+        // make the run() method throw an exception
+        $ex = new Exception('foo');
+        WorkingTestScript::$throw = $ex;
+        $this->expectExceptionObject($ex);
+        $instance->run();
     }
 }
